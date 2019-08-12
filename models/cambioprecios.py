6482e174a88ce469f0c2a6459f4ae67dc2fba8ea
fitrival <fitrivalapp@gmail.com>
2019-08-12 17:28:23 -0500

    Se comenta el archivo cambioprecios

--- models/cambioprecios.py
+++ models/cambioprecios.py
@@ -2,21 +2,32 @@ from . import motgama
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError,Warning, ValidationError
 
+#   Se instancia la clase heredada de motgama.py
 class MotgamaWizardCambioPrecios(models.TransientModel):
     _inherit = 'motgama.wizardcambioprecios'
 
     @api.multi
+    #   Se define la función del botón de cambio de precios
     def btn_cambiar_precios(self):
-        # raise Warning('Esta función está en otro archivo')
+        #   Se cargan las listas de precios configuradas para las habitaciones
         ListasPreciosHabitacion = self.env['motgama.listapreciohabitacion'].search([])
         if ListasPreciosHabitacion.exists():
-            ListasPreciosHabitacion.unlink() # Si existen listas de precios: Borra toda la tabla de lista de precios de Habitación
+            #   Si existe al menos una lista de precios: Borra toda la tabla de lista de precios de Habitación
+            ListasPreciosHabitacion.unlink()
+
+        #   Se cargan los tipos de habitación
         TiposHabitacion = self.env['motgama.tipo'].search([])
+
+        #   Se recorre cada uno de los tipos
         for tipo in TiposHabitacion:
+            #   Se cargan las listas de precios y las habitaciones del tipo de habitación
             Listas = self.env['motgama.listapreciotipo'].search([('tipo_id','=',tipo.id)])
             Habitaciones = self.env['motgama.habitacion'].search([('tipo_id','=',tipo.id)])
+
+            #   Se recorre cada habitación y cada lista de precios
             for habitacion in Habitaciones:
                 for lista in Listas:
+                    #   Se carga una variable con la información de la lista de precios
                     infoLista = {
                         'nombrelista':lista.nombrelista,
                         'tarifaocasional':lista.tarifaocasional,
@@ -25,4 +36,8 @@ class MotgamaWizardCambioPrecios(models.TransientModel):
                         'active':lista.active,
                         'habitacion_id':habitacion.id
                     }
+
+                    #   Se graba la información cargada de la lista de precios del tipo en la tabla de lista de precios de habitación
                     self.env['motgama.listapreciohabitacion'].create(infoLista)
+        
+                    #   TODO: Salir de la vista de actualizar precios
